e4fda7caf05476d29b06dbd4b8dcba209d0366d4
Songphan Choemprayong <songphan@gmail.com>
2025-01-07 12:01:47 +0700

    Create 4_employeDB_UPDATERECORD.sql

---
+++ SQL/1 Data Definition and Manipulation/4_employeDB_UPDATERECORD.sql
@@ -0,0 +1,13 @@
+--Update a record:
+
+UPDATE employee
+	SET departmentid=130
+	WHERE employeeid=4444;
+
+--Delete a record or multiple records:
+
+DELETE FROM department
+	WHERE departmentid=129;
+	
+
+
